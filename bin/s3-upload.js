ee8136e08d41278ead20856959aa7de301801140
wmcb91 <wmcb91@gmail.com>
2016-10-13 15:02:32 -0400

    Determine file/mime-type
    Use the file-type module to determine the content type of the upload
    (mime-type).
    
    Define a function that gives us a default file-type and mime-type in
    the event that the file-type is given and unsupported in the
    file-type module.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
 
 const filename = process.argv[2] || '';
 
@@ -15,10 +16,26 @@ const readFile = (filename) => {
   });
 };
 
-const logMessage = (data) => {
-  console.log(`${filename} is ${data.length} bytes long`);
+// return a default object in case the filetype given is unsupport for fileType
+// to read.
+const mimeType = (data) => {
+  return Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream',
+  }, fileType(data));
+};
+
+const parseFile = (fileBuffer) => {
+  let file = mimeType(fileBuffer);
+  file.data = fileBuffer;
+  return file;
+};
+
+const logMessage = (file) => {
+  console.log(`${filename} is ${file.data.length} bytes long and is of mime ${file.mime}`);
 };
 
 readFile(filename)
+.then(parseFile)
 .then(logMessage)
 .catch(console.error);
