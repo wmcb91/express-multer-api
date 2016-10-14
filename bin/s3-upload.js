f9f8afbf159e73e6ba4a4d4d45e2ad0ad9889b9b
wmcb91 <wmcb91@gmail.com>
2016-10-14 11:46:31 -0400

    Name file uploads using crypto module
    - Define wrapper functions for naming the file and the directory

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,10 +1,11 @@
 'use strict';
 
 // this has to come before anything else
-// we evaluate .env and get it into this script
 require('dotenv').config();
 
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
 const AWS = require('aws-sdk');
 
@@ -16,13 +17,14 @@ const readFile = (filename) => {
       if (error) {
         reject(error);
       }
+
       resolve(data);
     });
   });
 };
 
-// return a default object in case the filetype given is unsupport for fileType
-// to read.
+// return a default object in the case that fileType is given an unsupported
+// filetype to read
 const mimeType = (data) => {
   return Object.assign({
     ext: 'bin',
@@ -36,6 +38,31 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
+};
+
+const nameFile = (file) => {
+  return randomHexString(16)
+  .then((val) => {
+    file.name = val;
+    return file;
+  });
+};
+
+const nameDirectory = (file) => {
+  file.dir = new Date().toISOString().split('T')[0];
+  return file;
+};
+
 const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -45,7 +72,7 @@ const s3 = new AWS.S3({
 
 const upload = (file) => {
   const options = {
-    // get the bucket name from the AWS S3 console
+    // get the bucket name from your AWS S3 console
     Bucket: 'will-wdi-bucket',
     // attach the fileBuffer as a stream to send to S3
     Body: file.data,
@@ -54,7 +81,7 @@ const upload = (file) => {
     // tell S3 what the mime-type is
     ContentType: file.mime,
     // pick a filename for S3 to use for the upload
-    Key: `test/test.${file.ext}`
+    Key: `${file.dir}/${file.name}.${file.ext}`
   };
 
   return new Promise((resolve, reject) => {
@@ -62,18 +89,22 @@ const upload = (file) => {
       if (error) {
         reject(error);
       }
+
       resolve(data);
     });
   });
 };
 
 const logMessage = (response) => {
-  // turn the pojo into a string so I can read it in the console
+  // turn the pojo into a string so I can see it on the console
   console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
 .then(upload)
 .then(logMessage)
-.catch(console.error);
+.catch(console.error)
+;
